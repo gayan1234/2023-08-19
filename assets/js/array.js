0807a90590f7360c6e97fff0749fe7604acd1bb5
VM Holidays <gayanchathuranga1992@gmail.com>
2023-08-29 09:53:47 +0530

    array

---
+++ assets/js/array.js
@@ -0,0 +1,19 @@
+const imgs = ['image-1.jpg', 'bg/image.png', 'products.jpg']
+console.log(imgs[0])
+console.log(imgs[2])
+imgs[1] = 'image-2.png'
+console.log(imgs)
+
+const cars = ['BMW', 'Nissan', 'Tesla', 'Toyota', 'Audi']
+console.log(cars.toString())
+console.log(cars.length)
+
+console.log(cars)
+cars.sort()
+console.log(cars)
+
+console.log(cars.join('-'))
+
+const cars2 = ['Ford', 'TATA']
+
+//pop/push/shift/unshift/delete
