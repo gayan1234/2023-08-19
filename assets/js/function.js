fd0be3f5b1ff56044b7f140b73e1ec5d0b6679ab
VM Holidays <gayanchathuranga1992@gmail.com>
2023-08-30 10:53:50 +0530

    function

---
+++ assets/js/function.js
@@ -0,0 +1,19 @@
+function addTwoNumbers(a,b) {
+    console.log(a+b)
+    return a+b;
+}
+
+function getSqRoot(a) {
+    console.log(Math.sqrt(a))
+    return Math.sqrt(a);
+}
+
+function getCercumference(r) {
+    //2*PI*r
+    console.log(2*Math.PI*r)
+    return 2*Math.PI*r;
+}
+
+addTwoNumbers(5,7)
+getSqRoot(5)
+getCercumference(14)
