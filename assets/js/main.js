ffb2408799ede8e3b6537d9501fdc7b8041922a7
VM Holidays <gayanchathuranga1992@gmail.com>
2023-08-31 09:32:08 +0530

    c2f and f2c

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,24 @@
+const c = document.querySelector('#celsius')
+const f = document.querySelector('#fahrenheit')
+const c2f = document.querySelector('#c2f')
+const f2c = document.querySelector('#f2c')
+
+c2f.addEventListener('click', () => {
+    if(c.value == "") {
+        alert("Please enter a value")
+        return
+    }
+    f.value = (c.value * 9/5) + 32
+})
+
+f2c.addEventListener('click', () => {
+    if(f.value == "") {
+        alert("Please enter a value")
+        return
+    }
+    c.value = (f.value - 32) * 5/9
+})
+
 /*let btn = document.querySelector("#btn")
 let out = document.querySelector("#out")
 
@@ -36,18 +57,18 @@ console.log(str1 +' '+ str2)
 console.log(str1.concat(' ', str3,' ',str2))
 */
 
-const text = document.querySelector('#text')
-const find = document.querySelector('#find')
-const replace = document.querySelector('#replace')
-const btnFR = document.querySelector('#btnFR')
-const outFR = document.querySelector('#outFR')
+// const text = document.querySelector('#text')
+// const find = document.querySelector('#find')
+// const replace = document.querySelector('#replace')
+// const btnFR = document.querySelector('#btnFR')
+// const outFR = document.querySelector('#outFR')
 
-// Change first text
-btnFR.addEventListener('click',function(){
-    let output = text.value
-    output = output.replace(find.value, replace.value)
-    outFR.innerText = output
-})
+// // Change first text
+// btnFR.addEventListener('click',function(){
+//     let output = text.value
+//     output = output.replace(find.value, replace.value)
+//     outFR.innerText = output
+// })
 
 // Change all text
 /*btnFR.addEventListener('click',function(){
@@ -58,13 +79,16 @@ btnFR.addEventListener('click',function(){
 */
 
 // Change all text, Text tranform and Trim
-btnFR.addEventListener('click',function(){
-    let findText = find.value
-    findText = findText.trim();
+// btnFR.addEventListener('click',function(){
+//     let findText = find.value
+//     findText = findText.trim();
 
-    let output = text.value
+//     let output = text.value
 
-    output = output.replaceAll(findText, replace.value.trim())
-    outFR.innerText = output
-    //console.log(output.toUpperCase())
-})
+//     output = output.replaceAll(findText, replace.value.trim())
+//     outFR.innerText = output
+//     //console.log(output.toUpperCase())
+// })
+
+
+
