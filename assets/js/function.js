b27d44a05ba726a89b100d1a1710f0c0e73870c4
VM Holidays <gayanchathuranga1992@gmail.com>
2023-08-31 09:09:43 +0530

    function

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,19 +1,40 @@
 function addTwoNumbers(a,b) {
-    console.log(a+b)
     return a+b;
 }
 
-function getSqRoot(a) {
-    console.log(Math.sqrt(a))
-    return Math.sqrt(a);
-}
+// function getSqRoot(a) {
+//     console.log(Math.sqrt(a))
+//     return Math.sqrt(a);
+// }
 
 function getCercumference(r) {
     //2*PI*r
-    console.log(2*Math.PI*r)
+    //console.log(2*Math.PI*r)
     return 2*Math.PI*r;
 }
 
-addTwoNumbers(5,7)
-getSqRoot(5)
-getCercumference(14)
+let circumference = getCercumference(5)
+
+console.log(circumference)
+
+
+//Arrow function
+const getArea = (radius) => {
+    return Math.PI*radius*radius;
+}
+
+console.log(getArea(5))
+
+
+//Scope
+let lastName = "Doe"
+
+function myFunction() {
+    console.log("Hello World")
+    let firstName = "Jhon"
+    console.log(firstName)
+    console.log(lastName)
+}
+myFunction()
+console.log(lastName)
+console.log(firstName)
